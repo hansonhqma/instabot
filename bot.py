ec3127d50f716e3580fbceafa103df475db77c66
hansonma <hanson.hq.ma@gmail.com>
2020-07-04 01:05:28 -0700

    comment func

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 class bot:
     def __init__(self, USERNAME, PASSWORD):
@@ -9,6 +10,16 @@ class bot:
         self.USERNAME = USERNAME
         self.PASSWORD = PASSWORD
         self.landing_page = r'https://www.instagram.com'
+        self.browser = webdriver.Chrome('DRIVERS/chromedriver')
+        try:
+            self.browser.get(self.landing_page)
+            time.sleep(3)
+            self.browser.find_element_by_name("username").send_keys(self.USERNAME)
+            PWD_ELEM = self.browser.find_element_by_name("password")
+            PWD_ELEM.send_keys(self.PASSWORD)
+            PWD_ELEM.submit()
+        except Exception as e:
+            print("Error logging into account:", e)
 
     def comment(self, text, link):
         if not (isinstance(text, str) and isinstance(link, str)):
@@ -20,4 +31,24 @@ class bot:
 
         if not 'instagram.com' in link.lower():
             print("Not an Instagram link")
-
+            return
+        try:
+            self.browser.get(link)
+        except Exception as e:
+            print("Unable to access post:", e)
+            return
+        try:
+            COMMENT_BOX = self.browser.find_element(By.CLASS_NAME, "Ypffh") # finish here
+        except Exception as e:
+            print("Error accessing comment section:", e)
+            return
+        try:
+            COMMENT_BOX.send_keys(text)
+        except Exception as e:
+            print("Error sending text to comment field:", e)
+            return
+        try:
+            COMMENT_BOX.submit()
+        except Exception as e:
+            print("Error submitting text:", e)
+            return
